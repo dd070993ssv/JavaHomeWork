eb07dd19a67d614a9b61f1efff388c99e6b0ce84
susoev <susoev071993@gmail.com>
2021-10-13 16:38:02 +0300

    init commit

--- src/com/pb/susoev/hw2/Calculator.java
+++ src/com/pb/susoev/hw2/Calculator.java
@@ -6,19 +6,63 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
 
-        int x;
-        int y;
-        String znak;
+        int operand1 = 0;
+       int operand2 = 0;
+        String sign;
+      int rezult;
 
-        System.out.println("vvedite cifru1");
-        x=scan.nextInt();
-        System.out.println("vvedite cifru2");
-        y=scan.nextInt();
-        System.out.println("vvedite znak");
-        znak= scan.next();
-
-
-        System.out.println(x+y+znak);
+        System.out.println("введите цифру №1");
+       try {
+           operand1=scan.nextInt();
+       }
+        catch (Exception ex1){
+            System.out.println("Это не цифра");
+            return;
+        }
+        System.out.println("введите цифру №2");
+        try {
+            operand2=scan.nextInt();
+        }
+        catch (Exception ex2){
+            System.out.println("Это не цифра");
+            return;
+        }
+        System.out.println("введите знак операции");
+        sign= scan.next();
+        //  if (!sign.equals("/")){
+        //       if (!sign.equals("*")){
+        //            if (!sign.equals("+")){
+        //                if (!sign.equals("-")){
+        //                      System.out.println("ошибка ввода знака");
+        //                }
+        //            }
+        //        }
+        //    }
+        switch (sign){
+            case "+":
+                rezult=operand1+operand2;
+                System.out.println("Результат="+rezult);
+            break;
+            case "-":
+                rezult=operand1-operand2;
+                System.out.println("Результат="+rezult);
+                break;
+            case "*":
+                rezult=operand1*operand2;
+                System.out.println("Результат="+rezult);
+                break;
+            case "/":
+          try {
+              rezult=operand1/operand2;
+          }
+          catch (ArithmeticException ex3 ) {
+              System.out.println("Ошибка деления на 0");
+              return;
+          }
+                System.out.println("Результат="+rezult);
+                break;
+            default: System.out.println("ошибка ввода знака");
+        }
 
     }
 }
