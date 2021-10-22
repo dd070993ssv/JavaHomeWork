dd0e810fdd7f8894876960df482e8adf0d8e16aa
susoev <susoev071993@gmail.com>
2021-10-22 14:45:44 +0300

    init commit

---
+++ src/com/pb/susoev/hw3/Array.java
@@ -0,0 +1,61 @@
+package com.pb.susoev.hw3;
+//import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] array = new int[5];
+        int[] array2 = array;
+        int sum = 0;
+        int count=0;
+        boolean w = true;
+        String mas ;
+//ввод масива
+     /*   for (int i = 0; i < array.length; i++) {
+            array[i] = input.nextInt();
+        }*/
+        while (w) {
+            System.out.println("Введите масив с целых чисел:");
+            try {
+                for (int i = 0; i < array.length; i++) {
+                    mas = input.next();
+                    array[i] = Integer.parseInt(mas.trim());
+                   w=false;}continue;
+            } catch (Exception ex1) {
+                System.out.println("Неверно введенный масив");
+                continue;
+            }}
+//вывод первого масива
+        System.out.print ("Введенный масив:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print (" " + array[i]);
+        }
+        System.out.print ("\n");
+
+        int sort;
+        for (int i = array2.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array2[j] > array2[j + 1]) {
+                    sort = array2[j];
+                    array2[j] = array2[j + 1];
+                    array2[j + 1] = sort;
+                }
+            }
+        }
+//вывод второго масива
+        System.out.print ("Отсортированый масив:");
+        for (int i = 0; i < array2.length; i++) {
+            System.out.print (" " + array2[i]);
+        }
+        System.out.print ("\n");
+
+        //сумма первого масива
+        for (int i = 0; i < array.length; i++) {
+           sum=sum+array[i];
+           if (array[i]>0) count++;
+        }
+        System.out.print ("Сумма всех элементов: "+sum+"\n");
+        System.out.print ("Количество положительных элементов: "+count+"\n");
+        }
+    }
--- src/com/pb/susoev/hw3/Bingo.java
+++ src/com/pb/susoev/hw3/Bingo.java
@@ -1,14 +1,16 @@
 package com.pb.susoev.hw3;
 
+import java.text.Format;
 import java.util.Scanner;
 
 public class Bingo {
     public static void main(String[] args) {
         System.out.println("Игра началась!!!\nОтгадай число, вводя свою цифру от 0 до 100");
         Scanner scan = new Scanner(System.in);
-    //    Scanner line = new Scanner(System.in);
+        //    Scanner line = new Scanner(System.in);
+        String cr;
         int cufra;
-        int q = (int) (Math.random() * 2);
+        int q = (int) (Math.random() * 101);
         int count = 0;
         boolean w = true;
 
@@ -16,39 +18,41 @@ public class Bingo {
         while (w) {
             System.out.println("Введите цифру:");
             try {
-                cufra = scan.nextInt();
+                cr = scan.next();
+                cufra = Integer.parseInt(cr.trim());
+                System.out.println(cufra);
             } catch (Exception ex1) {
                 System.out.println("Это не цифра");
-                return;
+                continue;
             }
             count++;
-            if (cufra<0){
+            if (cufra < 0) {
                 System.out.println("Ваша цифра с знаком минус. Мое загаданное цисло от 0 до 100");
-            } else
-            if (cufra > q) {
+            } else if (cufra > q) {
                 System.out.println("Твоя цифра больше моей");
             }
-            if (cufra < q&& cufra >=0) {
+            if (cufra < q && cufra >= 0) {
                 System.out.println("Твоя цифра меньше моей");
             }
             if (cufra == q) {
-                w=false;
-                System.out.println("Красавчик. Ты выиграл!\nКоличество твоих попыток="+count);
+                w = false;
+                System.out.println("Красавчик. Ты выиграл!\nКоличество твоих попыток=" + count);
             }
-            if(w){
-                System.out.println("Хотите продолжать?\nВведите символ 'y' - хочу или 'n' - остановить ");
-                String ln;
-                ln = scan.next();
-                if (ln.equals("n")){
-                    w=false;
-                    System.out.println("Програма остановлена");
-                } else
-                if (!ln.equals("y")){
-                   System.out.println("ошибка ввода знака");
-                  return;
-            }
+            if (w) {
+                    System.out.println("Если хотите продолжать нажмите любую клавишу\nВведите символ 'n' - если хотите остановить!");
+                    String ln;
+                    ln = scan.next();
+                    if (ln.equals("n")) {
+                        w = false;
+                        System.out.println("Програма остановлена");
+                    }
+          //              continue;
+                    }
+
             }
 
         }
-        }
+
     }
+
+
