f7493b71ba3d737fbd92a4920ef89a6c55afc2c7
susoev <susoev071993@gmail.com>
2021-11-22 21:48:31 +0200

    init commit

--- src/com/pb/susoev/hw9/FileNumbers.java
+++ src/com/pb/susoev/hw9/FileNumbers.java
@@ -1,24 +1,58 @@
 package com.pb.susoev.hw9;
 
-import java.io.FileWriter;
-import java.io.Writer;
-import java.security.SecureRandom;
+import java.io.*;
 
 public class FileNumbers {
-    public static void main(String[] args)  {
-        for (int i=0;i<100;i++){
+    private static void createNumbersFile() throws IOException {
+        Writer writer = new FileWriter("files/numbers.txt");
+        for (int i = 1; i < 101; i++) {
             int q = (int) (Math.random() * 100);
-            System.out.println(q);
-     //   String data = "This is the data in the output file";
-
-        // Creates a Writer using FileWriter
-        try (Writer writer = new FileWriter("files/s02-writer.txt")) {
-
-            // Writes string to the file
-            writer.write(q);
-
-        } catch (Exception e) {
-            e.getStackTrace();
+            try {
+                writer.write(String.valueOf(q));
+                if (i % 10 == 0) {
+                    writer.write("\n");
+                } else {
+                    writer.write(" ");
+                }
+            } catch (Exception e) {
+                e.getStackTrace();
+            }
         }
-    }}
+        writer.close();
+    }
+
+    private static void createOddNumbersFile ()throws IOException{
+        BufferedReader reader = new BufferedReader(new FileReader("files/numbers.txt"));
+        Writer writer = new FileWriter("files/odd-numbers.txt");
+        for(int i=0;i<10;i++) {
+            String line = reader.readLine();
+            String[] array = line.split(" ");
+            for (int u = 0; u < 10; u++) {
+                Integer q = Integer.parseInt(array[u]);
+                if (q % 2 == 0) {
+                    writer.write("0");
+                } else {
+                    writer.write(String.valueOf(q));
+                }
+                writer.write(" ");
+            }
+
+            writer.write("\n");
+        }
+        writer.close();
+        reader.close();
+    }
+
+    public static void main(String[] args) {
+        try {
+            createNumbersFile();
+            System.out.println("File numbers.txt create down");
+            createOddNumbersFile();
+            System.out.println("File numbers.txt writer down. File odd-numbers.txt create down");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
